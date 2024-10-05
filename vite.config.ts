7b559b8941f7c03a7f70f0d10e7791b707c2a283
Alexey Alekhin <laughedelic@gmail.com>
2024-10-05 06:21:57 +0200

    simplify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,9 @@ import type { UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import glsl from 'vite-plugin-glsl';
 
-// https://vitejs.dev/config
-export default {
+export default { // https://vitejs.dev/config
   plugins: [
     react(),
     glsl(),
   ],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 9000,
-  },
 } satisfies UserConfig;
